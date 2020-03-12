0d049b5f15918db1267617a19c5ddc3c06d30609
lockam1 <lockam1@student.op.ac.nz>
2020-03-13 10:58:00 +1300

    Added server files

---
+++ server/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, url_for
+from mongoengine import *
+
+connect('countries')
+
+app = Flask(__name__)
+
+class Country(Document):
+    name = StringField()
+    population = StringField()
+    
+
+
+
+@app.route('/')
+@app.route('/home')
+@app.route('/index')
+def index():
+    Country(name='New Zealand', population='5 million').save()
+    return render_template("index.html")
+
+
+
+@app.route("/inspiration")
+def inspiration():
+    return render_template("inspiration.html")
+
+
+@app.route("/testCountry")
+def listCountryTest():
+    
+    return Country.objects.to_json()
+  
+
+if __name__ =="__main__":
+    app.run(host='0.0.0.0', port=8080)
+	
+
+
