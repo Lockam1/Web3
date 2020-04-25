fb8dc27aedaba99433f491c4e62e8841ab7cc73a
lockam1 <anthonylock19@gmail.com>
2020-04-25 16:06:56 +1200

    Working on d3

--- server/static/js/d3.js
+++ server/static/js/d3.js
@@ -1,8 +1,7 @@
-d3.select("p").style("color", "green")
-
-d3.select("body")
-  .selectAll("p")
-  .data([4, 8, 15, 16, 23, 42])
-  .enter().append("p")
+d3.select("p")
+    .data([4, 8, 15, 16, 23, 42])
+    .enter().append("p")
     .text(function(d) { return "I’m number " + d + "!"; });
+
+
   
