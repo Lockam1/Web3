c3d4222316987df398d963405e572e5cf359b01d
lockam1 <anthonylock19@gmail.com>
2020-04-25 15:52:12 +1200

    Working on d3

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -2,16 +2,6 @@ $(function(){
 	$('button').click(function(){  //fuction trigger by success '200' code
         alert('clicked');
         $("div[id='ajax']").remove();
-
-        var bodySelection = d3.select("body");
-        var svgSelection = bodySelection.append("svg")
-            .attr("width", 50)
-            .attr("height", 50);
-        var circleSelection = svgSelection.append("circle")
-            .attr("cx", 25)
-            .attr("cy", 25)
-            .attr("r", 25)
-            .style("fill", "purple");
     
     }).fail(function(){   //function trigger by fail '400', '500' code
 
@@ -20,8 +10,4 @@ $(function(){
     })
 });
 
-var paragraphs = document.getElementsByTagName("p");
-for (var i = 0; i < paragraphs.length; i++) {
-  var paragraph = paragraphs.item(i);
-  paragraph.style.setProperty("color", "blue", null);
-}
+
