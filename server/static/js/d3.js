1dd17cea0a47c4fd5cca715f06fa9b5069be0cc7
lockam1 <anthonylock19@gmail.com>
2020-04-25 16:21:34 +1200

    Working on d3

--- server/static/js/d3.js
+++ server/static/js/d3.js
@@ -1,7 +1,9 @@
-d3.select("p").transition()
-    .style("color", "green")
+d3.select("p").style("color", "green")
 
 // d3.select("body").transition()
     
-  
-  
+
+values = Array(5) [1, 3, -4, 2, 4]
+
+import {chart as chart1} from "@ds/histogram" 
+
