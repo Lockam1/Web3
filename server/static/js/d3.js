bd5e6176c005a1c63dd682bd15fbc3393035c316
lockam1 <anthonylock19@gmail.com>
2020-04-25 16:04:37 +1200

    Working on d3

--- server/static/js/d3.js
+++ server/static/js/d3.js
@@ -1,50 +1,8 @@
 d3.select("p").style("color", "green")
 
-
-chart = {
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height])
-        .attr("stroke-width", 2);
-  
-    const circles = d3.range(20).map(i => ({
-      x: Math.random() * (width - radius * 2) + radius,
-      y: Math.random() * (height - radius * 2) + radius,
-    }));
-  
-    svg.selectAll("circle")
-      .data(circles)
-      .join("circle")
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y)
-        .attr("r", radius)
-        .attr("fill", (d, i) => d3.schemeCategory10[i % 10])
-        .call(drag);
-  
-    return svg.node();
-  }
-  drag = ƒ(t)
-
-  drag = {
-
-    function dragstarted(d) {
-      d3.select(this).raise().attr("stroke", "black");
-    }
-  
-    function dragged(d) {
-      d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
-    }
-  
-    function dragended(d) {
-      d3.select(this).attr("stroke", null);
-    }
-  
-    return d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
-  }
-
-  height = 600
-  radius = 32
-  
+d3.select("body")
+  .selectAll("p")
+  .data([4, 8, 15, 16, 23, 42])
+  .enter().append("p")
+    .text(function(d) { return "I’m number " + d + "!"; });
   
