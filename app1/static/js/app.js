65070193c5c46af6d9a7b95ef9940556cc7d9c70
lockam1 <lockam1@student.op.ac.nz>
2020-06-04 13:18:18 +1200

    Added a FarmBot temp file

--- app1/static/js/app.js
+++ app1/static/js/app.js
@@ -139,6 +139,39 @@ function graph(){
 
     // add a text to each 'g'
     en.append("text").text(function(d){ return d.name });
+   
+    //Tesing axis
+    var width = 400, height = 100;
+
+    var data = [10, 15, 20, 25, 30];
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var xscale = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, width - 100]);
+
+    var yscale = d3.scaleLinear()
+            .domain([0, d3.max(data)])
+            .range([height/2, 0]);
+
+    var x_axis = d3.axisBottom()
+            .scale(xscale);
+
+    var y_axis = d3.axisLeft()
+            .scale(yscale);
+
+        svg.append("g")
+        .attr("transform", "translate(50, 10)")
+        .call(y_axis);
+
+    var xAxisTranslate = height/2 + 10;
+
+        svg.append("g")
+                .attr("transform", "translate(50, " + xAxisTranslate  +")")
+                .call(x_axis)
 
 };
    
