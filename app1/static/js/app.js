cbcb14055f283e278671a41199133ae7358b1a8b
lockam1 <anthonylock19@gmail.com>
2020-06-11 20:23:06 +1200

    Working on axis for app1

--- app1/static/js/app.js
+++ app1/static/js/app.js
@@ -189,7 +189,7 @@ function graph(){
 function graph2(){
     $("svg#data").empty();
     var g = d3.select("#axis").append("svg").attr("id", "data").selectAll("g").data(csvFile1)
-        .attr("padding-left", "40");
+        .attr("padding-left", "70");
     var x = 0;
     var drawX;
     var population2radius = d3.scaleSqrt() // instead of scaleLinear()
@@ -200,7 +200,7 @@ function graph2(){
         .attr("transform",function(d, i){ 
         x = file[i].data.aged_25_54_labour_force_participation_rate_percent[year];
         drawX = x * 5;
-        return "translate("+ (drawX) + 100 + "," + (600 - (population2radius(popTotal[i].data.population_total[year])/5) + 40 +")" 
+        return "translate("+ (drawX) + 100 + "," + ((600 - (population2radius(popTotal[i].data.population_total[year]) / 5)) + 70 +")" 
         //if population is above 400 million place at top of graph
         });
     //add a circle to each 'g'
