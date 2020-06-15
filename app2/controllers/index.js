708832ecc427fa4c58f0381c1c8b18520b17e15b
lockam1 <anthonylock19@gmail.com>
2020-06-15 16:47:09 +1200

    Updating App2

--- app2/controllers/index.js
+++ app2/controllers/index.js
@@ -1,4 +1,4 @@
-const apiCallFromHttp = require('../src/httpRequest.js');
+var apiCallFromHttp = require('../src/httpRequest.js');
 
 
 //Routes for /... links
@@ -10,10 +10,12 @@ module.exports = {
     return res.status(200).render('about');
   },
   request(req, res){
-      apiCallFromHttp.callApi(function (response) {
-        res.write(JSON.stringify(response));
-        res.end();
-      });
+      //require('../src/httpRequest.js')()
+      apiCallFromHttp.getData()
+      // apiCallFromHttp.https(function (response) {
+      //   res.write(JSON.stringify(response));
+      //   res.end();
+      // });
     return res.status(200).render('index');
   }
 }
--- app2/src/httpRequest.js
+++ app2/src/httpRequest.js
@@ -1,20 +1,25 @@
 const https = require('https');
 
-https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-  let data = '';
+function getData() {
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+        let data = '';
 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            try {
+                let json = JSON.parse(body);
+                // do something with JSON
+            } catch (error) {
+                console.error(error.message);
+            };
+        });
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
-module.exports.callApi = callExternalAPIHttp;
+        }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        });
+};
