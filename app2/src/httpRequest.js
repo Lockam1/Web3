069a2841a2727c718693b41a180de7da6a7b69f5
lockam1 <lockam1@student.op.ac.nz>
2020-06-16 16:42:14 +1200

    Working on app2

--- app2/src/httpRequest.js
+++ app2/src/httpRequest.js
@@ -1,8 +1,18 @@
 const fetch = require("node-fetch");
+const Window = require('window');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+// const LocalStorage = require('node-localstorage').LocalStorage;
+const window = new Window();
+global.document = new JSDOM(html).window.document;
+// localStorage = new LocalStorage('./scratch');
+
 var usableData;
+var temp;
 
 //Calling the URL from App1 which has the json data.
-var url = "http://192.168.1.156:80/csv4";
+var url = "http://10.25.138.110:80/csv1"; //OP network
+var url1 = "http://192.168.1.156:80/csv4"; //Home network
 // $.getJSON(url, function(data) {
 // //   console.log("Data from App1")
 // //   console.log(data)
@@ -10,19 +20,45 @@ var url = "http://192.168.1.156:80/csv4";
 //     usableData = localStorage.getItem('data'); 
 //     console.log('UsableData: ', JSON.parse(usableData));
 // });
+window.onload = getData();
 
-fetch("http://192.168.1.156:80/csv4")
-    .then(res => res.json())
-    .then(data => usableData = data)
-    .then(() => console.log(usableData));
-    // localStorage.setItem('data', JSON.stringify(tmep));
-    // usableData = localStorage.getItem('data'); 
+async function getData(){
+    const json = await fetch('http://10.25.138.110:80/csv1')
+        .then(response => response.json());
+    console.log(json);
+    
+    //loop for adding the countries to the drop down selection.
+    var select = document.getElementById('select2');
+
+    for (var i = 0; i < json.length; i++){
+        select.innerHTML = select.innerHTML +
+            '<option value="' + json[i]['id'] + '">' + json[i]['name'] + '</option>'
+    }
+}
+
+
+    // window.localStorage.setItem('data', JSON.stringify(temp));
+    // usableData = window.localStorage.getItem('data'); 
     // console.log('UsableData: ', JSON.parse(usableData));
 
 
+//Reading data from link take 1
+// const response = await fetch("http://10.25.138.110:80/csv1")
+//     .then(res => res.json())
+//     .then(data => temp = data)
+//     .then(() => console.log(temp));
+// console.log(response);
+
+//Reading data from link take 2
+// const json = await fetch('http://10.25.138.110:80/csv1')
+//     .then(response => response.json());
+// console.log(json);
+
+//Local storage setup and saving
 // localStorage.setItem('data', JSON.stringify(tmep));
 // usableData = localStorage.getItem('data'); 
 // console.log('UsableData: ', JSON.parse(usableData));
+
 //loop for adding the countries to the drop down selection.
 // var select = document.getElementById('select2');
 // for (var i =0; i < usableData.length; i++){
